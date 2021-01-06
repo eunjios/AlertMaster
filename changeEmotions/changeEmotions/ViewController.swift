e0e1b068c34977611df22270804ba07f4b205d86
Eunji Lee <77034159+eunjios@users.noreply.github.com>
2021-01-06 11:34:41 +0900

    확인 버튼만 있는 alert 창
    handler: .nil

---
+++ changeEmotions/changeEmotions/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  changeEmotions
+//
+//  Created by 이은지 on 2021/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var a = "angry"
+    var b = "happy"
+    
+    
+    @IBOutlet weak var emotionLabel: UILabel!
+    @IBOutlet weak var emotionImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emotionLabel.text = "\(a)!!"
+        emotionImage.image = UIImage(named: "\(a).jpg")
+    }
+    
+    
+    @IBAction func showAlert(_ sender: Any) {
+        let message = "기분이 '\(a)' 에서 '\(b)' 로 바뀌었습니다."
+        
+        let alert = UIAlertController(title: "Change", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+        (a, b) = (b, a)
+        emotionLabel.text = "\(a)!!"
+        emotionImage.image = UIImage(named: "\(a).jpg")
+    }
+    
+}
+
